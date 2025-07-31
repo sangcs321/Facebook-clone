711318184085d3009f38964e07e414d9c5a6c501
21130511@st.hcmuaf.edu.vn <21130511@st.hcmuaf.edu.vn>
2025-07-31 10:57:17 +0700

    fix alias import link

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,14 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@Components': path.resolve(__dirname, 'src/Components'),
+      '@Redux': path.resolve(__dirname, 'src/Redux'),
+      '@Store': path.resolve(__dirname, 'src/Redux/Store/Store'),
+      '@Pages': path.resolve(__dirname, 'src/Pages'),
+      '@Layouts': path.resolve(__dirname, 'src/Layouts'),
+      '@RTKQuery': path.resolve(__dirname, 'src/Redux/RTKQuery'),
+      '@Constants': path.resolve(__dirname, 'src/Constants'),
+      '@Slice': path.resolve(__dirname, 'src/Redux/Slice'),
     },
   },
 };
